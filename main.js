77749396b07f81c3e1a7cb47ec6bd9c2536e22b1
neperj <your_email@example.com>
2025-02-07 02:15:36 +0000

    g

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-// Add your JavaScript code here
-console.log('Hello from the main.js file!');
+// Generate the secret key and public key
+let sk = window.NostrTools.generateSecretKey(); // `sk` is a Uint8Array
+let pk;
+
+// Wait for 2 seconds before generating and displaying the public key
+setTimeout(() => {
+  pk = window.NostrTools.getPublicKey(sk);
+  console.log(sk);
+  console.log(pk);
+
+  // Display the public key on the HTML
+  const pkDisplayElement = document.getElementById("pk-display");
+  pkDisplayElement.textContent = `Key: ${pk}`;
+}, 2000);
